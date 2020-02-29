c1662bc55f3a4e4a1bf59817cb237413bf1b46ab
Dania King <dania@fortunebuilders.com>
2020-02-29 14:32:11 -0800

    Everything is really working all clicks and buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -124,7 +124,6 @@ $( '#search' ).on( 'click', function ()
 
 
         let but = $( '<button>' ).text( topics[ nt ] ).attr( 'class', "btn btn-primary click" ).attr( 'data-search', topics[ nt ] );
-
         $( '#buttonDiv' ).append( but );
 
 
@@ -134,23 +133,45 @@ $( '#search' ).on( 'click', function ()
 
 } )
 
+$( document ).on( 'click', '.click', function ()
+{
+    $( '#resultsDiv' ).empty();
+    let search = $( this ).attr( 'data-search' );
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=SIzTkCeV8ao2iGwLRXzhrwanEImChuJP&limit=10";
 
-
-// Remake buttons from updted array
-
-
-
-
-
-
-
-
-
-
-
+    $.ajax( {
+        url: queryURL,
+        method: "GET"
+    } ).then( function ( response )
+    {
 
 
 
+        var results = response.data
+        for ( let d = 0; d < results.length; d++ )
+        {
 
-// on search
-// take the value push it into topics, create button, make ajax call
+            // Create gifDiv and set the attributes to call it and animate later
+            var gifDiv = $( '<div>' );
+
+            var rating = results[ d ].rating;
+
+            var p = $( '<p>' ).text( 'Rating: ' + rating );
+            var giph = $( "<img>" );
+            giph.attr( 'class', 'moveMe' );
+            giph.attr( 'data-still', results[ d ].images.original_still.url );
+            giph.attr( 'data-move', results[ d ].images.original.url )
+            giph.attr( 'data-state', 'still' );
+            giph.attr( "src", results[ d ].images.fixed_width_still.url );
+            gifDiv.append( p, giph );
+            $( '#resultsDiv' ).prepend( gifDiv );
+
+
+        }
+
+
+
+
+
+    } )
+} )   
